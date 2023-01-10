de20d02606d217f43d542cba3a5a870843566e60
Dysprosium <dysprosium@outlook.com.br>
2023-01-10 12:13:38 -0300

    :dart: Feature: Implementar Tela de Gerenciamento de Receitas

--- src/app/page/receita/receita-cadastrar/receita-cadastrar.page.ts
+++ src/app/page/receita/receita-cadastrar/receita-cadastrar.page.ts
@@ -7,12 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReceitaCadastrarPage implements OnInit {
 
+  public isRecebido: boolean = true;
+  public iconeCoracao: string = "heart-outline";
+
   constructor() { }
 
   ngOnInit() { }
 
   public apresentarMaisDetalhes() {
+    console.log("apresentarMaisDetalhes...");
+  }
 
+  public abrirCategoriaDespesa() {
+    console.log("abrirCategoriaDespesa...");
+  }
+
+  public abrirContaBancaria() {
+    console.log("abrirContaBancaria...");
+  }
+
+  public abrirAnexos() {
+    console.log("abrirAnexos...");
+  }
+
+  public verificarValorRecebido(event: any) {
+    if (this.isRecebido) {
+      this.isRecebido = false;
+    } else {
+      this.isRecebido = true;
+    }
+  }
+
+  public adicionarPessoa() {
+    console.log("adicionarPessoa...");
+    if (this.iconeCoracao == "heart-outline") {
+      this.iconeCoracao = "heart";
+    } else {
+      this.iconeCoracao = "heart-outline";
+    }
+    debugger
   }
 
 }
