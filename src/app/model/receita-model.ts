f451618a7bc6980b3c50ec070b5000a32de6811c
Dysprosium <dysprosium@outlook.com.br>
2023-01-11 13:49:04 -0300

    :dart: Feature: Implementar Tela de Gerenciamento de Receitas

---
+++ src/app/model/receita-model.ts
@@ -0,0 +1,7 @@
+export class ReceitaModel {
+    public codigo: string = "";
+    public valorTotal: number = 0;
+    public dataRecebimento: Date | undefined;
+    public pessoaFavorecida: string = "";
+    public categoriaLancamentoFinanceiro: string = "";
+}
